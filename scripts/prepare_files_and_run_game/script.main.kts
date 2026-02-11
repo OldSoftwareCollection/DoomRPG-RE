d34ac4963d1c589dbb8f39326c00c2fab4dbd6c6
Autumn Fall <cryphun96@gmail.com>
2026-02-12 00:53:12 +0300

    Enhanced first step in script, working on enhancing second
    Started working on enhancing prompt

--- scripts/prepare_files_and_run_game/script.main.kts
+++ scripts/prepare_files_and_run_game/script.main.kts
@@ -4,44 +4,11 @@ import java.util.zip.ZipEntry
 import java.util.zip.ZipInputStream
 import java.nio.file.Files
 
-fun download(urlString: String, dest: File) {
-    URL(urlString).openStream().use { input ->
-        FileOutputStream(dest).use { output ->
-            input.copyTo(output)
-        }
-    }
-}
-
-fun unzip(zipFile: File, destDir: File) {
-    destDir.mkdirs()
-    ZipInputStream(FileInputStream(zipFile)).use { zis ->
-        var entry: ZipEntry? = zis.nextEntry
-        while (entry != null) {
-            val newFile = File(destDir, entry.name)
-            if (entry.isDirectory) {
-                newFile.mkdirs()
-            } else {
-                newFile.parentFile.mkdirs()
-                FileOutputStream(newFile).use { fos ->
-                    zis.copyTo(fos)
-                }
-            }
-            entry = zis.nextEntry
-        }
-    }
-}
 
 fun main() {
-    val currentDir = System.getProperty("user.dir")
-    val tempFolder = File(currentDir, "Temporary")
-    tempFolder.mkdir()
-    val brewBinaryFolder = File(tempFolder, "Brew Binary")
-    brewBinaryFolder.mkdir()
-    val desktopBinaryFolder = File(tempFolder, "Desktop Binary")
-    desktopBinaryFolder.mkdir()
-    val filesFolder = File(tempFolder, "Files")
-    filesFolder.mkdir()
 
+
+    /*
     val brewUrl = "https://archive.org/download/doomrpg_brew/doomrpg.zip"
     val brewFile = File(filesFolder, "doomrpg.zip")
     download(brewUrl, brewFile)
@@ -101,4 +68,32 @@ fun main() {
     val pbDoomRPG = ProcessBuilder("DoomRPG.exe")
     pbDoomRPG.directory(gameFolder)
     pbDoomRPG.start().waitFor()
+    */
+}
+
+fun download(urlString: String, dest: File) {
+    URL(urlString).openStream().use { input ->
+        FileOutputStream(dest).use { output ->
+            input.copyTo(output)
+        }
+    }
+}
+
+fun unzip(zipFile: File, destDir: File) {
+    destDir.mkdirs()
+    ZipInputStream(FileInputStream(zipFile)).use { zis ->
+        var entry: ZipEntry? = zis.nextEntry
+        while (entry != null) {
+            val newFile = File(destDir, entry.name)
+            if (entry.isDirectory) {
+                newFile.mkdirs()
+            } else {
+                newFile.parentFile.mkdirs()
+                FileOutputStream(newFile).use { fos ->
+                    zis.copyTo(fos)
+                }
+            }
+            entry = zis.nextEntry
+        }
+    }
 }
